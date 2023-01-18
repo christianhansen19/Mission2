d4c74dfac81cfc69f8f36a75e8d32cc08b2dc600
christianhansen <christianhansen@CHRISTIANHA8F87>
2023-01-17 20:04:52 -0700

    Add project files.

---
+++ Mission2pt2/PrintStuff.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Mission2pt2
+{
+    public class PrintStuff
+    {
+        // Constructor
+        public PrintStuff ()
+        {
+
+        }
+        public void PrintResults(int numRolls, int[] totals)
+        {
+            Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
+            Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
+            Console.WriteLine("Total number of rolls = " + numRolls + ".");
+            Console.WriteLine();
+
+            // Histogram
+            for (int i = 2; i <= 12; i++)
+            {
+                Console.Write(i + ": ");
+                int numStars = (int)((double)totals[i] / numRolls * 100);
+                for (int j = 0; j < numStars; j++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye!");
+        }
+    }
+}
---
+++ Mission2pt2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Mission2pt2
+{
+
+   public class DiceSimulation
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the dice throwing simulator!");
+            Console.Write("How many dice rolls would you like to simulate? ");
+            int numRolls = int.Parse(Console.ReadLine());
+
+            int[] totals = new int[13];
+
+            // Dice Roll
+            Random rnd = new Random();
+            for (int i = 0; i < numRolls; i++)
+            {
+                int die1 = rnd.Next(1, 7);
+                int die2 = rnd.Next(1, 7);
+                int total = die1 + die2;
+                totals[total]++;
+            }
+
+            PrintStuff ps = new PrintStuff(); //Declaration and Instantiation
+
+            ps.PrintResults(numRolls, totals);
+        }
+    }
+
+}
